c545bbe22fdbaaef5c41802849cd4812fc3e50c0
tatsumack <tatsu.mack@gmail.com>
2019-10-12 11:03:59 +0900

    Resolve the conflict with another extension

--- google_extension/js/main.js
+++ google_extension/js/main.js
@@ -17,7 +17,7 @@ $(function () {
     function draw(inUrl, outUrl) {
         if (!inUrl || !outUrl) return;
 
-        $("#main-container > div.row > div:nth-child(2) > div:nth-child(12) > table > tbody > tr > td:nth-child(1)").each(function () {
+        $("#main-container > div.row > div:nth-child(2) > div:nth-last-child(1) > table > tbody > tr > td:nth-child(1)").each(function () {
             var testCaseName = $(this).text();
             var fileName = getFileName(testCaseName);
             $(this).append(" [ <a href='" + inUrl + "?preview=" + fileName.in + "'>in</a> / <a href='" + outUrl + "?preview=" + fileName.out + "'>out</a> ]");
